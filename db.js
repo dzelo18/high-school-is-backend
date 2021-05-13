cd87ca13fff3c892f6cb9a9384a3ebc9dbccce13
dzelo18 <dzelo18@epoka.edu.al>
2021-05-13 12:52:35 +0200

    Set up test database

--- db.js
+++ db.js
@@ -1,6 +1,12 @@
 var mysql = require('mysql2');
-var settings = require('./dbSettings.json');
 var db;
+
+var settings = {
+    host     : process.env.DB_HOST,
+    user     : process.env.DB_USER,
+    password : process.env.DB_PASSWORD,
+    database : process.env.DB_DATABASE
+}
 
 function connectDatabase() {
     if (!db) {
@@ -11,6 +17,7 @@ function connectDatabase() {
                 console.log('Database is connected!');
             } else {
                 console.log('Error connecting database!');
+                console.log(err);
             }
         });
     }
