118c7421096261f5848068ccb5320ba63e68b7a1
unknown <panci.ar@panci-electronic.com>
2021-05-15 20:46:51 +0200

    Changed hashing algorithm

--- controllers/AuthenticationController.js
+++ controllers/AuthenticationController.js
@@ -1,15 +1,81 @@
 const jwt = require('jsonwebtoken');
 const yup = require('yup');
-const bcrypt = require('bcrypt');
+const sha256 = require('sha256');
 const db = require('../db.js');
 const crypto = require('crypto');
 
 let secret = process.env.TOKEN_SECRET;
 
-const signUpSchema = yup.object({
+const credentialSchema = yup.object({
 	email: yup.string().required(),
 	password: yup.string().required()
 });
+
+exports.authorizeRequest = function(req, res, next) {
+	let header = req.headers['authorization'];
+	if (!header) res.status(401).send('Unauthorized Request!');
+
+	const token = header.split(' ')[1];
+	if (!token) res.status(401).send("Unauthorized Request!");
+
+	jwt.verify(token, secret, (err, data) => {
+		if (err) res.status(401).send("Couldn't validate token!");
+		let userID = data.userID;
+		if (!userID) res.status(401).send("Invalid Token!");
+		let sql = 'SELECT COUNT(*) AS accountCount FROM USER WHERE userID=?';
+		db.query(sql, [userID], (err, result) => {
+			if (err) res.status(500).json({ response: err.message });
+			if (result[0].accountCount == 0) res.status(401).send("Invalid supplied account!");
+		});
+		next();
+	});
+}
+
+exports.signup = async function(req, res) {
+	let body = req.body;
+
+	let accCredentials = body.credentials;
+
+	try {
+		if (!credentialSchema.isValidSync(accCredentials)) {
+			throw new Error("Invalid data format supplied!");
+        }
+        
+		let userId;
+		let hashedPassword = sha256(accCredentials.password);
+
+		let sql = `INSERT INTO User (role, username, password) VALUES ("student", ?, ?)`;
+
+		await db.promise().query(sql, [accCredentials.email, hashedPassword]).then(([rows, fields]) => {
+			userId = rows.insertId;
+		}).catch((err) => {
+			throw new Error(err);
+		});
+
+		let tokenIdentifier = crypto.randomBytes(16).toString('hex');
+		req.session.tokId = tokenIdentifier;
+		req.session.userId = userId;
+
+		let accessToken = jwt.sign({ user: userId }, secret, { expiresIn: '1h' });
+		let refreshToken = jwt.sign({ tokenId: tokenIdentifier }, secret, { expiresIn: '1h' });
+
+		res.cookie("refresh_token", JSON.stringify({
+			token: refreshToken
+		}), {
+			httpOnly: true,
+		});
+
+		res.status(201).send({
+			message: "Account created successfully!",
+			token: accessToken,
+			accountData: {
+				userId: userId,
+			}
+		});
+	} catch (ex) {
+		res.status(400).json({ message: ex.message });
+	}
+}
 
 exports.signIn = async function(req, res) {
 	let body = req.body;
@@ -17,16 +83,16 @@ exports.signIn = async function(req, res) {
 
 	try {
 
-		if (!signUpSchema.isValidSync(accCredentials)) {
+		if (!credentialSchema.isValidSync(accCredentials)) {
 			throw new Error("Invalid data format supplied!");
 		}
 
 		let sql = 'SELECT * FROM User WHERE username=? AND password=?'
-		//let hashedPassword = await bcrypt.hash(accCredentials.password, 10);
+		let hashedPassword = sha256(accCredentials.password);
 
 		let userId;
 
-		await db.promise().query(sql, [accCredentials.email, accCredentials.password]).then(([rows, fields]) => {
+		await db.promise().query(sql, [accCredentials.email, hashedPassword]).then(([rows, fields]) => {
             if(rows.length == 0) throw new Error("No user was found with the provided credentials!");
             userId = rows[0]['userID'];
 		}).catch((err) => {
@@ -58,71 +124,6 @@ exports.signIn = async function(req, res) {
 		res.status(400).json({ message: ex.message });
 	}
 
-}
-
-exports.authorizeRequest = function(req, res, next) {
-	let header = req.headers['authorization'];
-	if (!header) res.status(401).send('Unauthorized Request!');
-
-	const token = header.split(' ')[1];
-	if (!token) res.status(401).send("Unauthorized Request!");
-
-	jwt.verify(token, secret, (err, data) => {
-		if (err) res.status(401).send("Couldn't validate token!");
-		let userID = data.userID;
-		if (!userID) res.status(401).send("Invalid Token!");
-		let sql = 'SELECT COUNT(*) AS accountCount FROM USER WHERE userID=?';
-		db.query(sql, [userID], (err, result) => {
-			if (err) res.status(500).json({ response: err.message });
-			if (result[0].accountCount == 0) res.status(401).send("Invalid supplied account!");
-		});
-		next();
-	});
-}
-
-exports.signup = async function(req, res) {
-	let body = req.body;
-
-	let accountCredentials = body.credentials;
-
-	try {
-		if (!signUpSchema.isValidSync(accountCredentials)) {
-			throw new Error("Invalid data format supplied!");
-		}
-		let userId;
-		//let hashedPassword = await bcrypt.hash(accountCredentials.password, 10);
-
-		let sql = `INSERT INTO User (role, username, password) VALUES ("student", ?, ?)`;
-
-		await db.promise().query(sql, [accountCredentials.email, accountCredentials.password]).then(([rows, fields]) => {
-			userId = rows.insertId;
-		}).catch((err) => {
-			throw new Error(err);
-		});
-
-		let tokenIdentifier = crypto.randomBytes(16).toString('hex');
-		req.session.tokId = tokenIdentifier;
-		req.session.userId = userId;
-
-		let accessToken = jwt.sign({ user: userId }, secret, { expiresIn: '1h' });
-		let refreshToken = jwt.sign({ tokenId: tokenIdentifier }, secret, { expiresIn: '1h' });
-
-		res.cookie("refresh_token", JSON.stringify({
-			token: refreshToken
-		}), {
-			httpOnly: true,
-		});
-
-		res.status(201).send({
-			message: "Account created successfully!",
-			token: accessToken,
-			accountData: {
-				userId: userId,
-			}
-		});
-	} catch (ex) {
-		res.status(400).json({ message: ex.message });
-	}
 }
 
 exports.refreshToken = function(req, res) {
