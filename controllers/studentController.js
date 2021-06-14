062f3f7ae8240541822e4028ca8815d52bff2f11
Ergys Rrjolli <rrjolligys@gmail.com>
2021-06-14 23:09:59 +0200

    Linked the course page to the database

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -17,7 +17,9 @@ exports.getCourses = function (req, res) {
 };
 
 exports.getTimetableForStudent = function (req, res) {
-    let sql = 'SELECT * FROM Timetable WHERE Timetable.courseID IN (SELECT courseID FROM CourseSelections WHERE studentID=?)';
+    let sql = `SELECT t.slot, t.day, t.courseID, c.name as courseName
+	FROM Timetable as t, Course as c, CourseSelections as cs 
+	WHERE t.courseID=c.courseID and cs.courseID=c.courseID and cs.studentID=?`;
     let id = encryptionUtils.decrypt(req.params.id);
     db.query(sql, [id], (err, result) => {
         if(err) {
@@ -41,18 +43,6 @@ exports.getAssignmentsForStudent = function (req, res) {
     });
 };
 
-exports.getAttendanceForStudent = function (req, res) {
-    let sql = `SELECT * FROM Attendance WHERE studentID = ? AND courseID IN (SELECT courseID FROM CourseSelections WHERE studentID=?)`;
-    let id = encryptionUtils.decrypt(req.params.id);
-    db.query(sql, [id, id], (err, result) => {
-        if(err) {
-            return res.status(500).send(utils.buildResponse("error", {}, err.message));
-        } else {
-            return res.status(200).send(utils.buildResponse("success", result, ""));
-        }
-    });
-};
-
 exports.getGradesForStudent = function (req, res) {
     console.log("ARRIVED AT STUDENT MIDDLEWARE!");
     let sql = `SELECT * FROM Grade WHERE studentID = ? AND courseID IN (SELECT courseID FROM CourseSelections WHERE studentID=?)`;
@@ -66,7 +56,8 @@ exports.getGradesForStudent = function (req, res) {
     });
 };
 
-exports.getAverageGradeForStudent = function (req, res) {
+exports.getAverageGradeForStudent = function (req, res, next) {
+	req.statistics = {};
     let sql = `SELECT grade FROM Grade WHERE studentID = ?`;
     let id = encryptionUtils.decrypt(req.params.id);
     db.query(sql, [id], (err, result) => {
@@ -78,56 +69,67 @@ exports.getAverageGradeForStudent = function (req, res) {
 
             for(let i=0; i<nrOfGrades; i++) sum += result[i].grade;
             let average = sum / nrOfGrades;
-
-            res.status(200).send(utils.buildResponse("success", {average: average}, ""));
+			req.statistics.average = average;
+			next();
         }
     });
 }
 
-exports.getMaximumGradeForStudent = function (req, res) {
+exports.getMaximumGradeForStudent = function (req, res, next) {
     let sql = `SELECT Grade.grade, Course.name FROM Grade,Course WHERE studentID = ? AND Grade.courseID=Course.courseID ORDER BY Grade.grade DESC LIMIT 1`;
     let id = encryptionUtils.decrypt(req.params.id);
     db.query(sql, [id], (err, result) => {
         if(err) {
             res.status(500).send(utils.buildResponse("error", {}, err.message));
         } else {
-            res.status(200).send(utils.buildResponse("success", {maxGrade: result[0].grade, subject: result[0].name}, ""));
+			req.statistics.maxGrade = result[0].grade;
+			req.statistics.maxSubject = result[0].name;
+			next();
         }
     });
 }
 
-exports.getMinimumGradeForStudent = function (req, res) {
+exports.getMinimumGradeForStudent = function (req, res, next) {
     let sql = `SELECT Grade.grade, Course.name FROM Grade,Course WHERE studentID = ? AND Grade.courseID=Course.courseID ORDER BY Grade.grade ASC LIMIT 1`;
     let id = encryptionUtils.decrypt(req.params.id);
     db.query(sql, [id], (err, result) => {
         if(err) {
             res.status(500).send(utils.buildResponse("error", {}, err.message));
         } else {
-            res.status(200).send(utils.buildResponse("success", {minGrade: result[0].grade, subject: result[0].name}, ""));
+			req.statistics.minGrade = result[0].grade;
+			req.statistics.minSubject = result[0].name;
+			next();
         }
     });
 }
 
-exports.getLatestGradeForStudent = function (req, res) {
+exports.getLatestGradeForStudent = function (req, res, next) {
     let sql = `SELECT Grade.grade, Course.name, Grade.date FROM Grade,Course WHERE studentID = ? AND Grade.courseID=Course.courseID ORDER BY Grade.date DESC LIMIT 1`;
     let id = encryptionUtils.decrypt(req.params.id);
     db.query(sql, [id], (err, result) => {
         if(err) {
             res.status(500).send(utils.buildResponse("error", {}, err.message));
         } else {
-            res.status(200).send(utils.buildResponse("success", {latestGrade: result[0].grade, subject: result[0].name}, ""));
+			req.statistics.latestGrade = result[0].grade;
+			req.statistics.latestSubject = result[0].name;
+			next();
         }
     });
 }
 
-exports.getCourseGradesForStudent = function (req, res) {
-    let sql = `SELECT * FROM Grades WHERE studentID = ? AND courseID = ?`;
+exports.getAttendanceForStudent = function (req, res) {
+    let sql = `SELECT * FROM Attendance WHERE studentID = ? AND courseID IN (SELECT courseID FROM CourseSelections WHERE studentID=?)`;
     let id = encryptionUtils.decrypt(req.params.id);
-    db.query(sql, [id, id, req.params.courseId], (err, result) => {
+    db.query(sql, [id, id], (err, result) => {
         if(err) {
-            res.status(500).send(utils.buildResponse("error", {}, err.message));
+            return res.status(500).send(utils.buildResponse("error", {}, err.message));
         } else {
-            res.status(200).send(utils.buildResponse("success", result, ""));
+			req.statistics.attendance = result.length;
+			req.statistics.unjustified = 0;
+			for(let i=0; i<result.length; i++){
+				if(!result[i].justified) req.statistics.unjustified++;
+			}
+            res.status(200).send(utils.buildResponse("success", req.statistics, ""));
         }
     });
 };
@@ -136,6 +138,23 @@ exports.getCourseAttendanceForStudent = function (req, res) {
     let sql = `SELECT * FROM Attendance WHERE studentID = ? AND courseID = ?`;
     let id = encryptionUtils.decrypt(req.params.id);
     db.query(sql, [id, req.params.courseId], (err, result) => {
+        if(err) {
+            res.status(500).send(utils.buildResponse("error", {}, err.message));
+        } else {
+			req.statistics.attendance = result.length;
+			req.statistics.unjustified = 0;
+			for(let i=0; i<result.length; i++){
+				if(!result[i].justified) req.statistics.unjustified++;
+			}
+            res.status(200).send(utils.buildResponse("success", req.statistics, ""));
+        }
+    });
+};
+
+exports.getCourseGradesForStudent = function (req, res) {
+    let sql = `SELECT * FROM Grade WHERE studentID = ? AND courseID = ?`;
+    let id = encryptionUtils.decrypt(req.params.id);
+    db.query(sql, [id, id, req.params.courseId], (err, result) => {
         if(err) {
             res.status(500).send(utils.buildResponse("error", {}, err.message));
         } else {
